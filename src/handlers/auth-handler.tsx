eb5136097d652059ba197fbe17bdd851e86722ed
Hr810004 <hr810004@gmail.com>
2025-04-24 23:24:29 +0530

    added authhandler function

--- src/handlers/auth-handler.tsx
+++ src/handlers/auth-handler.tsx
@@ -1,4 +1,21 @@
+import Loaderpage from "@/routes/Loaderpage"
+import { useAuth, useUser } from "@clerk/clerk-react"
+import { useEffect, useState } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+
 const AuthHandler = () => {
+   const {isSignedIn} = useAuth()
+   const { user } = useUser()
+   const pathname = useLocation().pathname
+   const navigate = useNavigate()
+   const [loading, setloading] = useState()
+
+   useEffect(() => {
+   },[user,isSignedIn,navigate,pathname])
+   
+   if (loading) {
+      return <Loaderpage/>
+   }
    return null
 }
 
